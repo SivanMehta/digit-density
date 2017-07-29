d332c64c4fc226715e3e8cb69d22f8baf9f4fa7f
Sivan Mehta <Sivanmehta2@gmail.com>
2017-07-28 21:40:37 -0700

    create function to convert to a any base

--- generate-data.R
+++ generate-data.R
@@ -1,19 +1,34 @@
 library(dplyr)
 library(ggplot2)
 
+# converting to a base less than 10, because
+# adding letters gets arbitrary and dumb
+convert.to.base <- function(n, base) {
+  num <- n
+  result <- ""
+  max.base <- log(num, base) %/% 1
+  
+  for(i in max.base:0) {
+    place.value <- base ** i
+    digit <- num %/% place.value
+    num <- num - digit * place.value
+    result <- paste(result, digit, sep = "")
+  }
+  
+  return(result)
+}
+
+convert.vector.to.base <- function(v, base) {
+  sapply(v, convert.to.base, base)
+}
+
+1:10 %>% convert.vector.to.base(2)
+
 tibble(x = 1:10000) %>%
-  mutate(contains.10 = grepl("4", as.character(x))) %>%
-  mutate(contains.16 = grepl("4", base::`!.hexmode`(x))) %>%
-  mutate(contains.8 = grepl("4", base::`!.octmode`(x))) %>%
+  mutate(contains.10 = grepl("4", convert.vector.to.base(x, 10))) %>%
   mutate(before.10 = cumsum(contains.10)) %>%
-  mutate(before.16 = cumsum(contains.16)) %>%
-  mutate(before.8 = cumsum(contains.8)) %>%
   mutate(density.10 = before.10 / x) %>%
-  mutate(density.16 = before.16 / x) %>%
-  mutate(density.8 = before.8 / x) %>%
   ggplot() +
-  geom_line(aes(x = log(x), y = density.10), colour = 'red') +
-  geom_line(aes(x = log(x), y = density.16), colour = 'blue') +
-  geom_line(aes(x = log(x), y = density.8), colour = 'green') +
+  geom_line(aes(x = log(x), y = density.10), colour = 'blue') +
   labs(title = 'Proportion of previous numbers containing the number "4"', y = "proportion")
   
